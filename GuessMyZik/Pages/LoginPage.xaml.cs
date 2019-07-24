9194d17c9020457b1b93469367e18d4fab037f11
Nathan GAGNIARRE <Nathan.GAGNIARRE@diiage.org>
2019-07-25 00:22:33 +0200

    Design LoginPage Modifié, A FINIR (Modifié la navigation) et rajouter une frame de bienvenue)

--- GuessMyZik/Pages/LoginPage.xaml.cs
+++ GuessMyZik/Pages/LoginPage.xaml.cs
@@ -17,6 +17,7 @@ using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 using GuessMyZik.Pages.Frames;
 using Windows.ApplicationModel.Core;
+using Windows.UI;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,6 +28,9 @@ namespace GuessMyZik.Pages
     /// </summary>
     public sealed partial class LoginPage : Page
     {
+
+        private RegistrationFrame registrationFrame = new RegistrationFrame();
+
         public LoginPage()
         {
             this.InitializeComponent();
@@ -34,20 +38,38 @@ namespace GuessMyZik.Pages
             navigationLogin.Navigate(typeof(SignInFrame)); //Display the page SignInFrame into the frame navigationLogin
         }
 
-        private void BtnRight_Click(object sender, RoutedEventArgs e)
-        {
-            if((sender as Button).Content.ToString() == "Registration") //Check if the content of the button BtnRight is equal to Registration
-            {
-                navigationLogin.Navigate(typeof(RegistrationFrame), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight }); //Display the page RegistrationFrame into the frame navigationLogin with an animation (Page come with slide from right)
-            } else
-            {
-                
-            }
-        }
-
+      
         private void BtnExit_Click(object sender, RoutedEventArgs e)
         {
             CoreApplication.Exit(); //Close the application
+        }
+
+        private void BtnRight_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button).Content.ToString() == "Registration") //Check if the content of the button BtnRight is equal to Registration
+            {
+                navigationLogin.Navigate(typeof(RegistrationFrame), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight }); //Display the page RegistrationFrame into the frame navigationLogin with an animation (Page come with slide from right)
+                (sender as Button).Content = "Back";
+                btnLeft.Content = "Confirm";
+            }
+            else
+            {
+                navigationLogin.GoBack();
+                (sender as Button).Content = "Registration";
+                btnLeft.Content = "Sign In";
+            }
+        }
+
+        private void BtnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button).Content.ToString() == "Sign In") //Check if the content of the button Btnleft is equal to Sign In
+            {
+                
+            }
+            else
+            {
+                
+            }
         }
     }
 }

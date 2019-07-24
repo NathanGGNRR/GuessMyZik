9a31284c0669c5b279c59fff4e603458a3204b76
Nathan GAGNIARRE <Nathan.GAGNIARRE@diiage.org>
2019-07-24 18:59:23 +0200

    Design LoginPage Terminé

--- GuessMyZik/Pages/LoginPage.xaml.cs
+++ GuessMyZik/Pages/LoginPage.xaml.cs
@@ -15,6 +15,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using GuessMyZik.Pages.Frames;
+using Windows.ApplicationModel.Core;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,8 +31,24 @@ namespace GuessMyZik.Pages
         {
             this.InitializeComponent();
             loginPageOpacityStoryBoard.Begin(); //Start the storyboard called loginPageOpacityStoryBoard on LoginPage.xaml: animate opacity of the gridLoginPage from 0 to 1 in 1 second.
+            navigationLogin.Navigate(typeof(SignInFrame)); //Display the page SignInFrame into the frame navigationLogin
         }
 
+        private void BtnRight_Click(object sender, RoutedEventArgs e)
+        {
+            if((sender as Button).Content.ToString() == "Registration") //Check if the content of the button BtnRight is equal to Registration
+            {
+                navigationLogin.Navigate(typeof(RegistrationFrame), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight }); //Display the page RegistrationFrame into the frame navigationLogin with an animation (Page come with slide from right)
+            } else
+            {
+                
+            }
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            CoreApplication.Exit(); //Close the application
+        }
     }
 }
  
